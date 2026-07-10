937a75e200e1757abbb4137ceae77b56f61a15d2
tapeshchavle <tapeshchawle@gmail.com>
2026-07-10 18:23:26 +0530

    files added

---
+++ src/test/java/com/gcp_deployment/GcpDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a663d6a26ff25a5a19dc145b9eadb4a10e7206e9
tapeshchavle <tapeshchawle@gmail.com>
2026-07-10 18:23:26 +0530

    files added

---
+++ src/main/java/com/gcp_deployment/GcpDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.gcp_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gcp_deployment/controller/Greeting.java
@@ -0,0 +1,16 @@
+package com.gcp_deployment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Greeting {
+
+    @GetMapping
+    public String hello(){
+        return "Hello Tapesh";
+    }
+    
+}
